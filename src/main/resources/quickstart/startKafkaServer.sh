289330623e7be73c740ac534ac6ce14eb5956581
akdp <akdp@ak-ubuntu-machine>
2022-10-06 18:38:34 +0530

    added kafka start/stop scripts for quick start

---
+++ src/main/resources/quickstart/startKafkaServer.sh
@@ -0,0 +1,2 @@
+cd /home/ak/dev/kafka_2.12-3.2.0/
+bin/kafka-server-start.sh config/server.properties
---
+++ src/main/resources/quickstart/startZookeeper.sh
@@ -0,0 +1,2 @@
+cd /home/akdp/dev/kafka_2.12-3.2.0/
+bin/zookeeper-server-start.sh config/zookeeper.properties
